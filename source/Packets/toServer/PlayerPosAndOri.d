81c80aee3d3c1a6540ea45ef22668836eb34ff7b
Bunnyking <252477176+johnberzins-source@users.noreply.github.com>
2026-08-28 13:42:29 -0500

    added player position and orientation packet.

---
+++ source/Packets/toServer/PlayerPosAndOri.d
@@ -0,0 +1,34 @@
+module Packets.toServer.PlayerPosAndOri;
+
+class PlayerPosAndOri : Packet {
+    immutable ubyte packetID = 0x08;
+    ubyte playerID;
+    short x;
+    short y;
+    short z;
+    ubyte yaw;
+    ubyte pitch;
+
+    this(ubyte packetID, short x, short y, short z, ubyte yaw, ubyte pitch) {
+        this.packetID = packetID;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+
+    ubyte[] serialize() {
+        Appender!(ubyte[]) data;
+
+        data.put(packetID);
+        data.put(playerID);
+        appendBigEndian(data,x);
+        appendBigEndian(data,y);
+        appendBigEndian(data,z);
+        data.put(yaw);
+        data.put(pitch);
+
+        return data;
+    }
+}
