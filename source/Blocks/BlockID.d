255efe6195ec4de650d9f0c76b7de63e32966f89
Bunnyking <252477176+johnberzins-source@users.noreply.github.com>
2026-08-29 12:41:14 -0500

    added blocks and changed a few file names.

---
+++ source/Blocks/BlockID.d
@@ -0,0 +1,58 @@
+module Blocks.BlockID;
+
+import std;
+
+// Minecraft Classic (v0.30) Block IDs
+immutable ubyte air              = 0;
+immutable ubyte stone            = 1;
+immutable ubyte grass            = 2;
+immutable ubyte dirt             = 3;
+immutable ubyte cobblestone      = 4;
+immutable ubyte woodPlanks       = 5;
+immutable ubyte sapling          = 6;
+immutable ubyte bedrock          = 7;
+immutable ubyte waterFlowing     = 8;
+immutable ubyte waterStill       = 9;
+immutable ubyte lavaFlowing      = 10;
+immutable ubyte lavaStill        = 11;
+immutable ubyte sand             = 12;
+immutable ubyte gravel           = 13;
+immutable ubyte goldOre          = 14;
+immutable ubyte ironOre          = 15;
+immutable ubyte coalOre          = 16;
+immutable ubyte log              = 17;
+immutable ubyte leaves           = 18;
+immutable ubyte sponge           = 19;
+immutable ubyte glass            = 20;
+
+// Wool / Cloth Colors
+immutable ubyte woolWhite        = 21;
+immutable ubyte woolOrange       = 22;
+immutable ubyte woolMagenta      = 23;
+immutable ubyte woolLightBlue    = 24;
+immutable ubyte woolYellow       = 25;
+immutable ubyte woolLime         = 26;
+immutable ubyte woolPink         = 27;
+immutable ubyte woolGray         = 28;
+immutable ubyte woolLightGray    = 29;
+immutable ubyte woolCyan         = 30;
+immutable ubyte woolPurple       = 31;
+immutable ubyte woolBlue         = 32;
+immutable ubyte woolBrown        = 33;
+immutable ubyte woolGreen        = 34;
+immutable ubyte woolRed          = 35;
+immutable ubyte woolBlack        = 36;
+
+immutable ubyte yellowFlower     = 37;
+immutable ubyte redFlower        = 38;
+immutable ubyte brownMushroom    = 39;
+immutable ubyte redMushroom      = 40;
+immutable ubyte blockOfGold      = 41;
+immutable ubyte blockOfIron      = 42;
+immutable ubyte doubleSlab       = 43;
+immutable ubyte slab             = 44;
+immutable ubyte bricks           = 45;
+immutable ubyte tnt              = 46;
+immutable ubyte bookshelf        = 47;
+immutable ubyte mossyCobblestone = 48;
+immutable ubyte obsidian         = 49;
