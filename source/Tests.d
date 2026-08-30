0eb8c81556b284b237a3650808f714adf7e753da
B-unnyKing <252477176+B-unnyKing@users.noreply.github.com>
2026-08-30 16:51:32 -0500

    cleaned up and used the built int function for flat terrain.

--- source/Tests.d
+++ source/Tests.d
@@ -3,18 +3,18 @@ module Tests;
 import std;
 import server;
 import Level.Level;
-
+import Generator.Flat;
 import Blocks.BlockID;
 
 
 void testWorld() {
     string nameOfSave = "testWorld";
     auto world = new ClassicWorld(512,64,512);
-    world.setLayer(1, stone);
-
+    setFlatTerrain("world");
+    
     world.saveToFile(nameOfSave);
 
     readln();
 
-    
+
 }
